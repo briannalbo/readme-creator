9d6d97ca4822077bc984723319687b035cd029bb
unknown <brianna.albo@gmail.com>
2022-12-25 17:19:22 -0500

    fixes function to render url and badge; updates markdown stylying template

--- Develop/README.md
+++ Develop/README.md
@@ -1,36 +1,37 @@
-# title 
-
-Boost_Software_License_1.0
-![badge](https://img.shields.io/badge/license-Boost_Software_License_1.0-purple)
+# sdf
 
 
-  ## Description)
-  loopdy loop
+![badge](https://img.shields.io/badge/license-Apache_License_2.0-purple)
+
+
+  ## Description
+  dfs
 
   ## Table of Contents
-  [Installation](#installation)
-  [Usage](#usage)
-  [License](#license)
-  [Contributions](#contributions)
-  [Tests](#tests)
-  [Questions](#questions)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
   ## Installation
-  npm install
+  sdf
 
-  ## Use
-  install inquirer, run node index.js
+  ## Usage
+  sdf
 
   ## License
-  Boost Software License 1.0
-  Further information on this license can be found at: https://choosealicense.com/licenses/bsl-1.0
+This project is licensed under the Apache License 2.0.
+- For information about this license, please visit: https://choosealicense.com/licenses/apache-2.0
 
-  ## Contributions
-  they are welcome
+## Contributions
+  
 
-  ## Tests
-  there are currently no tests available
+## Tests
+  
 
   ## Questions
-  For any questions or concerns, please refer to my github account: https://github.com/briannalbo
-  Additionally, feel free to contact me via email: brianna.albo@gmail.com
+  For any questions or concerns, please refer to my github account or contact me via email:
+- https://github.com/sfd
+- sd
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,105 +1,134 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// var url = '';
-// var badge = '';
+
 
 function licenseURLs(data)  {
- 
 
-  if (data.license == 'none')
+  if (data.license === 'none')
    {url = '', badge = ''}
-  else if (data.license == 'Apache License 2.0'){
+  else if (data.license === 'Apache License 2.0'){
     url = 'apache-2.0', badge = 'Apache_License_2.0' }
-  else if (data.license == 'GNU General Public License v3.0'){
+  else if (data.license === 'GNU General Public License v3.0'){
     url = 'gpl-3.0', badge = 'GNU_General_Public_License_v3.0'}
-  else if (data.license == 'MIT License'){
-    url = 'mit', badge == 'MIT_License' }
-  else if (data.license == 'BSD 2-Clause "Simplified" License'){
-    url = 'bsd-2-clause', badge = 'BSD_2--Clause_"Simplified"_License'}
-  else if (data.license == 'BSD 3-Clause "New" or "Revised" License'){
+  else if (data.license === 'MIT License'){
+    url = 'mit', badge = 'MIT_License' }
+  else if (data.license === "BSD 2-Clause 'Simplified' License"){
+    url = 'bsd-2-clause', badge = "BSD_2--Clause_'Simplified'_License"}
+  else if (data.license === "BSD 3-Clause 'New' or 'Revised' License"){
     url = 'bsd-3-clause', badge = 'BSD_3--Clause_"New"_or_"Revised"_License'}
-  else if (data.license == 'Boost Software License 1.0') {
+  else if (data.license === 'Boost Software License 1.0') {
     url = 'bsl-1.0', badge = 'Boost_Software_License_1.0' }
-  else if (data.license == 'Creative Commons Zero v1.0 Universal') {
+  else if (data.license === 'Creative Commons Zero v1.0 Universal') {
     url = 'cc0-1.0', badge = 'Creative_Commons_Zero_v1.0_Universal' }
-  else if (data.license == 'Eclipse Public License 2.0') {
+  else if (data.license === 'Eclipse Public License 2.0') {
     url = 'epl-2.0', badge = 'Eclipse_Public_License_2.0'}
-  else if (data.license == 'GNU Affero General Public License v3.0') {
+  else if (data.license === 'GNU Affero General Public License v3.0') {
     url = 'agpl-3.0', badge = 'GNU_Affero_General_Public_License_v3.0'}
-  else if (data.license == 'GNU General Public License v2.0') {
+  else if (data.license === 'GNU General Public License v2.0') {
     url = 'gpl-2.0', badge = 'GNU_General_Public_License_v2.0'}
-  else if (data.license =='GNU Lesser General Public License v2.1') {
+  else if (data.license === 'GNU Lesser General Public License v2.1') {
     url = 'lgpl-2.1', badge = 'GNU_Lesser_General_Public_License_v2.1'}
-  else if (data.license == 'Mozilla Public License 2.0') {
+  else if (data.license === 'Mozilla Public License 2.0') {
     url = 'mpl-2.0', badge = 'Mozilla_Public_License_2.0'}
-  else if (data.license == 'The Unlicense') {
+  else if (data.license === 'The Unlicense') {
     url = 'unlicense', badge = 'The_Unlicense'}
 
-    return url, badge
+    return `${url} ${badge} `;
 };
-function renderLicenseBadge(data) {
-  if (data.license = 'none'){
-  console.log("true")
-  return ''
-  }
-  else {
-    return `\n[![badge](https://img.shields.io/badge/license-${data.license}-purple)]\n`;
-  }
-};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
+  if (data.license === 'none') {
+    return  `# ${data.projectTitle}
+
+      ## Description)
+      ${data.description}
+    
+      ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributions](#contributions)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+      ## Installation
+      ${data.installation}
+    
+      ## Use
+      ${data.usage}
+    
+      ## License
+      This project does not use a license.
+      
+    
+      ## Contributions
+      ${data.contribute}
+    
+      ## Tests
+      ${data.tests}
+    
+      ## Questions
+      For any questions or concerns, please refer to my github account or contact me via email: 
+      - https://github.com/${data.username}
+      - email: ${data.email}
+    ;`
+  }
+
+
+else {
+  
 
 licenseURLs(data)
 console.log(url)
+console.log(badge)
 
   
 return `# ${data.projectTitle}
 
-${licenseURLs(data)}
-![badge](https://img.shields.io/badge/license-${licenseURLs(badge)}-purple)
+
+![badge](https://img.shields.io/badge/license-${badge}-purple)
 
 
-  ## Description)
+  ## Description
   ${data.description}
 
   ## Table of Contents
-  [Installation](#installation)
-  [Usage](#usage)
-  [License](#license)
-  [Contributions](#contributions)
-  [Tests](#tests)
-  [Questions](#questions)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
   ## Installation
   ${data.installation}
 
-  ## Use
+  ## Usage
   ${data.usage}
 
   ## License
-  ${data.license}
-  Further information on this license can be found at: https://choosealicense.com/licenses/${url}
+This project is licensed under the ${data.license}.
+- For information about this license, please visit: https://choosealicense.com/licenses/${url}
 
-  ## Contributions
+## Contributions
   ${data.contribute}
 
-  ## Tests
+## Tests
   ${data.tests}
 
   ## Questions
-  For any questions or concerns, please refer to my github account: https://github.com/${data.username}
-  Additionally, feel free to contact me via email: ${data.email}
+  For any questions or concerns, please refer to my github account or contact me via email:
+- https://github.com/${data.username}
+- ${data.email}
 `;
-};
+} };
 
 module.exports = {generateMarkdown, licenseURLs};
